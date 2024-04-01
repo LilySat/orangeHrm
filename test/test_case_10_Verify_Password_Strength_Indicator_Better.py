1dde1292e73863e88cbdd8a8e93d6467f645126a
liliiasattarova <sat-lilya@yandex.ru>
2024-04-01 00:50:10 -0400

    Test cases 4-11 done. No changes to lessons 5-6

--- test/test_case_10_Verify_Password_Strength_Indicator_Better.py
+++ test/test_case_10_Verify_Password_Strength_Indicator_Better.py
@@ -7,3 +7,15 @@
 # 3. Observe the password strength indicator.
 # Expected Result:
 # The password strength indicator should display a 'Better' message.
+
+
+def test_case_10_verify_password_strength_indicator_better(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.set_password('AAA00000')
+    app.orangeHrm.popUp.set_confirm_password('AAA00000')
+    app.assert_that(app.orangeHrm.popUp.check_strength_indicator()).is_equal_to('Better')
--- test/test_case_11_Verify_Password_Strength_Indicator_Strongest.py
+++ test/test_case_11_Verify_Password_Strength_Indicator_Strongest.py
@@ -7,3 +7,15 @@
 # 3. Observe the password strength indicator.
 # Expected Result:
 # The password strength indicator should display a 'Strongest' message.
+
+
+def test_case_11_verify_password_strength_indicator_Strongest(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.set_password('Aa1!Aa1!')
+    app.orangeHrm.popUp.set_confirm_password('Aa1!Aa1!')
+    app.assert_that(app.orangeHrm.popUp.check_strength_indicator()).is_equal_to('Strongest')
