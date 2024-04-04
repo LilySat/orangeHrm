c22097e4117437124be8d86827c80f11c04a6687
liliiasattarova <sat-lilya@yandex.ru>
2024-04-04 12:15:01 -0400

    resolve conflicts after merge upstream and master

--- test/test_case_6_Verify_Password_Field_Required_Validation.py.py
+++ test/test_case_6_Verify_Password_Field_Required_Validation.py.py
@@ -85,3 +85,68 @@
 # 6. Observe the password strength indicator.
 # Expected Result:
 # The password strength indicator should display a 'Strongest' message.
+
+def test_case_6_1_Test_Password_Field_Required(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.click_on_save
+    app.assert_that(app.orangeHrm.popUp.password_required()).is_equal_to('Required')
+    app.assert_that(app.orangeHrm.popUp.confirm_password_required()).is_equal_to('Required')
+
+def test_case_6_2_verify_password_minimum_length_validation(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.set_password('1')
+    app.assert_that(app.orangeHrm.popUp.password_required()).is_equal_to('Your password should have at least 8 characters.')
+
+def test_case_6_3_verify_password_strength_indicator_very_weak(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.set_password('00000000')
+    app.orangeHrm.popUp.set_confirm_password('00000000')
+    app.assert_that(app.orangeHrm.popUp.check_strength_indicator()).is_equal_to('Very Weak')
+
+def test_case_6_4_verify_password_strength_indicator_weak(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.set_password('AA000000')
+    app.orangeHrm.popUp.set_confirm_password('AA000000')
+    app.assert_that(app.orangeHrm.popUp.check_strength_indicator()).is_equal_to('Weak')
+
+def test_case_6_5_verify_password_strength_indicator_better(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.set_password('AAA00000')
+    app.orangeHrm.popUp.set_confirm_password('AAA00000')
+    app.assert_that(app.orangeHrm.popUp.check_strength_indicator()).is_equal_to('Better')
+
+def test_case_6_6_verify_password_strength_indicator_Strongest(app):
+    app.orangeHrm.openUrl("https://portnov_administrator-trials712.orangehrmlive.com")
+    app.orangeHrm.login_to_the_application()
+    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
+    app.orangeHrm.sideMenu.click_on_side_menu_button('HR Administration')
+    app.orangeHrm.step.specified_element_is_present('div[id="systemUserDiv"]')
+    app.orangeHrm.hrAdministration.click_add_user()
+    app.orangeHrm.popUp.set_password('Aa1!Aa1!')
+    app.orangeHrm.popUp.set_confirm_password('Aa1!Aa1!')
+    app.assert_that(app.orangeHrm.popUp.check_strength_indicator()).is_equal_to('Strongest')
